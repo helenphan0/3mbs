f8a0a442c6e9e26c75efe19be26c1a08b69e8247
helenphan0 <helen.phan@gmail.com>
2016-09-16 18:21:25 -0400

    v14

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    'url' : 'mongodb://localhost/3mbs' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://tester:1234@ds033106.mlab.com:33106/3mbs' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
 
